b6ae929a67b99bed1e29515056b2b221d7cb7e60
hussanuggg <kalungihassan.comug>
2026-07-16 09:05:08 +0300

    Use Render PORT environment variable

--- index.js
+++ index.js
@@ -1139,7 +1139,7 @@ io.on('connection', (socket) => {
         }
     });
 });
-
+const PORT = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server Started on port ${PORT}`);
     // remove orphaned images on startup
